d7f4e09b92bcd83d99a87174a6f1248edb793e1d
Jesse Ighor <ighor.jesse@gmail.com>
2023-02-12 17:29:48 +0100

    refactoring

--- src/config.py
+++ src/config.py
@@ -9,12 +9,8 @@ DEBUG = ENV == "development"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 MONGODB_SETTINGS = [{
-        "db": "citizen_feedback",
-        "host": os.getenv("DATABASE_HOST"),
-        "port": os.getenv("DATABASE_PORT"),
-        "username": os.getenv("DATABASE_USERNAME"),
-        "password": os.getenv("DATABASE_PASSWORD"),
-        "alias": os.getenv("DATABASE_ALIAS", "default"),
+        "db": os.getenv("DATABASE_NAME"),
+        "host": f"{os.getenv('DATABASE_URL')}"
 }]
 
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
