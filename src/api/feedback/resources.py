f1a8c9f6eefcf673d2da26e38452fec4f27f0040
Jesse Ighor <ighor.jesse@gmail.com>
2023-02-13 20:18:16 +0100

    refactor feedback processor

--- src/api/feedback/resources.py
+++ src/api/feedback/resources.py
@@ -6,11 +6,33 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import current_user
 
 from src.commons.constants import ALLOWED_FILE_EXTENSIONS
-from src.feedback.helpers import validate_csv_header, feedback_issue_processor
+from src.feedback.helpers import validate_csv_header
 from src.feedback.service import FeedbackService
 
 
 class Feedback(Resource):
+    """Creation feedback
+
+    ---
+    post:
+      tags:
+        - api
+      summary: Create feedbacks
+      description: Process community feedback
+      requestBody:
+        content:
+          application/json:
+            schema:
+              FeedbackSchema
+      responses:
+        201:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  feedback: FeedbackSchema
+    """
     method_decorators = [jwt_required()]
     def allowed_file(self, filename):
         return '.' in filename and \
@@ -30,23 +52,13 @@ class Feedback(Resource):
         file_name = f'{datetime.timestamp(datetime.utcnow())}.csv'
         file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], file_name)
         feedback_file.save(file_path)
-        
-        data = {
-            "community": community_data,
-            "created_at": float(file_name.split('.')[0]),
-            "submitted_by": current_user.id,
-            "csv_path": file_path,
-        }
-        instance = FeedbackService().create_feedback(**data)
-
-        issues = []
-        issue_analysis = {}
-        for item in feedback_issue_processor(current_app.config['UPLOAD_FOLDER'], file_name):
-            if not item.get('age', None):
-                issue_analysis = item
-            else:
-                issues.append(item)
-        FeedbackService().update_feedback(
-            instance, issues=issues, issue_analysis=issue_analysis
+    
+        FeedbackService().process_feedback(
+            file_name, {
+                "community": community_data,
+                "created_at": float(file_name.split('.')[0]),
+                "submitted_by": current_user.id,
+                "csv_path": file_path
+            }
         )
         return {}, 201
--- src/feedback/service.py
+++ src/feedback/service.py
@@ -1,5 +1,7 @@
+from .helpers import feedback_issue_processor
 from .models import Feedback
 from .schemas import FeedbackSchema
+from src.config import UPLOAD_FOLDER
 
 class FeedbackService:
     model = Feedback
@@ -13,4 +15,18 @@ class FeedbackService:
     def update_feedback(self, instance, **fields):
         instance = self.schema().update(instance, fields)
         instance.save()
+        return instance
+    
+    def process_feedback(self, csv_file_name, data):
+        instance = self.create_feedback(**data)
+        issues = []
+        issue_analysis = {}
+        for item in feedback_issue_processor(UPLOAD_FOLDER, csv_file_name):
+            if not item.get('age', None):
+                issue_analysis = item
+            else:
+                issues.append(item)
+        self.update_feedback(
+            instance, issues=issues, issue_analysis=issue_analysis
+        )
         return instance
